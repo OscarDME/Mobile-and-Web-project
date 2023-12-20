505688835f4c2bf15172367ddfb343a134e55f02
Bruno Sánchez <bruno.toreto@hotmail.com>
2023-12-20 09:49:09 -0600

    Added Mobile App Folder Structure

--- Project/WEB/src/App.js
+++ Project/WEB/src/App.js
@@ -3,33 +3,38 @@ import { useMsal, MsalProvider, AuthenticatedTemplate,UnauthenticatedTemplate } 
 import { loginRequest } from './auth/authConfig';
 
 
+/**
+ * WrappedView component handles user authentication state and conditional rendering of content.
+ * Uses MSAL React hooks and components to check if user is authenticated,
+ * and show login/logout buttons accordingly.
+*/
 const WrappedView = () => {
-  const {instance} = useMsal();
+  const { instance } = useMsal();
   const activeAccount = instance.getActiveAccount();
 
   const handleRedirect = () => {
     instance.loginPopup({
       ...loginRequest,
-    prompt: 'create',
+      prompt: 'create',
     })
-    .catch((error) => console.log(error));
+      .catch((error) => console.log(error));
   };
 
-const handleLogout = () => {
-  instance.logoutPopup();
-}
+  const handleLogout = () => {
+    instance.logoutPopup();
+  }
 
 
   return (
     <div className="App">
       <AuthenticatedTemplate>
         {activeAccount ? (<div>Welcome {activeAccount.name}
-        <div>Want to log out?
-        <button onClick={handleLogout}>Logout</button>
-        </div> 
+          <div>Want to log out?
+            <button onClick={handleLogout}>Logout</button>
+          </div>
         </div>
         )
-        : (<div>Please login</div>)}
+          : (<div>Please login</div>)}
       </AuthenticatedTemplate>
       <UnauthenticatedTemplate>
         <button onClick={handleRedirect}>Login</button>
