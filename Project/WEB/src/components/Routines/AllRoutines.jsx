b8a177f05e430010bc7a767f904ad8abcfee79e3
Bruno Sánchez <bruno.toreto@hotmail.com>
2024-02-17 17:16:25 -0600

    Finished Front-End All routines on WEB

--- Project/WEB/src/components/Routines/AllRoutines.jsx
+++ Project/WEB/src/components/Routines/AllRoutines.jsx
@@ -1,17 +1,41 @@
 import React, { useState } from 'react';
 import { RoutinesCard } from "../DATA_ROUTINES";
 import '../../styles/Management.css';
+import {ToolTip} from '../ToolTip';
+import SelectFilter from '../SelectFilter';
 
 export default function AllRoutines() {
     const [searchTerm, setSearchTerm] = useState('');
     const [selectedRoutine, setSelectedRoutine] = useState(null);
     const [expandedRow, setExpandedRow] = useState(null);
     const [expandedDay, setExpandedDay] = useState(null);
+    const [difficultyFilter, setDifficultyFilter] = useState('');
+    const [daysFilter, setDaysFilter] = useState('');
 
+    const difficultyOptions = [
+      { value: 'Fácil', label: 'Fácil' },
+      { value: 'Medio', label: 'Medio' },
+      { value: 'Difícil', label: 'Difícil' }
+    ];
 
-    const filteredExercises = RoutinesCard.filter(routine =>
-      routine.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+    const daysPerWeekOptions = [
+      {value: '1', label: '1'},
+      {value: '2', label: '2'},
+      {value: '3', label: '3'},
+      {value: '4', label: '4'},
+      {value: '5', label: '5'},
+      {value: '6', label: '6'},
+      {value: '7', label: '7'},
+    ];
+
+    const filteredExercises = RoutinesCard.filter(routine => {
+      return (
+        routine.name.toLowerCase().includes(searchTerm.toLowerCase()) &&
+        (difficultyFilter ? routine.difficulty === difficultyFilter : true) &&
+        (daysFilter ? routine.daysPerWeek.toString() === daysFilter : true)
+      );
+    });
+    
 
     const getDifficultyClass = (difficulty) => {
       switch(difficulty) {
@@ -46,6 +70,27 @@ export default function AllRoutines() {
 
     return (
       <div className="container2">
+          <div className="filters-container">
+          <div className='filter-row'>
+          Filtrar por dificultad: 
+          <SelectFilter
+            value={difficultyFilter}
+            onChange={(e) => setDifficultyFilter(e.target.value)}
+            options={difficultyOptions}
+            defaultOption="Todas las dificultades"
+          />
+          </div>
+          <div className='filter-row'>
+          Filtrar por días por semana:
+          <SelectFilter
+            value={daysFilter}
+            onChange={(e) => setDaysFilter(e.target.value)}
+            options={daysPerWeekOptions}
+            defaultOption="Todos los días"
+          />
+          </div>
+
+    </div>
         <ul className='cardcontainer-colors'>
           {filteredExercises.map((routine) => (
             <li key={routine.id} className={`row ${getDifficultyClass(routine.difficulty)} ${((selectedRoutine && selectedRoutine.id === routine.id)) ? 'selected' : ''}`}>
@@ -71,7 +116,10 @@ export default function AllRoutines() {
                               <ul className={`exercise-list ${blockIndex % 2 === 0 ? 'exercise-even' : 'exercise-odd'}`}>
                                 {block.exercises.map((exercise, exerciseIndex) => (
                                   <li key={exerciseIndex} className='exercise-row'>
-                                    <h5>{exercise.exerciseId.name}</h5> 
+                                  <div className='exercise-name'><h5>{exercise.exerciseId.name}</h5><ToolTip muscles={exercise.exerciseId.muscles} difficulty={exercise.exerciseId.difficulty} material={exercise.exerciseId.material} type={exercise.exerciseId.type}>
+                                  <i class="bi bi-info-circle-fill info-icon"></i>
+                                  </ToolTip> 
+                                  </div>
                                     {(exercise.exerciseId.type === "Pesas" || exercise.exerciseId.type === "Peso corporal") && (
                                       <div>
                                       {exercise.sets} Sets, {exercise.reps} Repticiones, {exercise.restBetweenSets} segundos de descanso
