f2d545714eb6c4ec92ed1cbde88e2d7a1a343eee
Bruno Sánchez <bruno.toreto@hotmail.com>
2024-03-11 20:21:54 -0600

    Adding chat on mobile

--- Project/Mobile/App.js
+++ Project/Mobile/App.js
@@ -3,8 +3,9 @@ import Login from "./src/screens/Inicio/login";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import MainRoot from "./src/navigators/navigationRoutes";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import Time from "./src/screens/Questionnarie/TimeAndDaysForm"
-import Muscles from "./src/screens/Questionnarie/Muscles"
+import Time from "./src/screens/Questionnarie/TimeAndDaysForm";
+import Muscles from "./src/screens/Questionnarie/Muscles";
+import "./FirebaseConfig";
 
 export default function App() {
   return <MainRoot />;
--- Project/Mobile/src/screens/Chat/Chat.js
+++ Project/Mobile/src/screens/Chat/Chat.js
@@ -1,30 +1,62 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, FlatList } from 'react-native';
 import React, { useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import config from "../../utils/conf";
 
 
 const MainMenu = ({ navigation }) => {
-  
   const [oid, setOid] = useState("");
+  const [conversaciones, setConversaciones] = useState([]);
+
+
+  
   useEffect(() => {
     AsyncStorage.getItem("userOID")
       .then((value) => {
         if (value !== null) {
           setOid(value);
           console.log("OID obtenido:", value);
+          fetchConversaciones(value); // Llama a fetchConversaciones aquí
         }
       })
       .catch((error) => {
         console.error("Error al obtener el OID:", error);
       });
   }, []);
+  
 
+
+  const fetchConversaciones = (oid) => {
+    firestore()
+      .collection('conversaciones')
+      .where('participantes', 'array-contains', oid)
+      .orderBy('modificadoEn', 'desc') // Asegúrate de tener un índice para esta consulta en Firestore
+      .onSnapshot((querySnapshot) => {
+        const conversaciones = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setConversaciones(conversaciones);
+      }, (error) => {
+        console.log(error);
+      });
+  };
+
+  
   console.log("OID:", oid);
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Chat</Text>
+      <FlatList
+        data={conversaciones}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Text onPress={() => navigation.navigate('Chat', { conversationId: item.id })}>
+            Conversación con ID: {item.id} - Modificado en: {item.modificadoEn.toDate().toDateString()}
+          </Text>
+        )}
+      />
     </View>
   );
 };
