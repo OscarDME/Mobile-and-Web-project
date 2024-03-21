9be21b64c9ce8f2d47f506d7adb1c9b44c2321ad
Bruno Sánchez <bruno.toreto@hotmail.com>
2024-03-20 22:31:11 -0600

    Fixed training skip set

---
+++ Project/Mobile/src/screens/Progreso/IndividualBodyMeasure.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
+
+export default function IndividualBodyMeasure() {
+  return (
+    <Text>IndividualBodyMeasure</Text>
+  )
+}
